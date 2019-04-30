9947b311b28b2b6f3b1a588b48607321af9301ad
KostasAronis <aronis_k@yahoo.com>
2019-04-30 19:53:10 +0300

    imported jsoup

--- src/app/App.java
+++ src/app/App.java
@@ -1,7 +1,13 @@
 package app;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello Java");
+        String html = "<html><head><title>First parse</title></head>"
+        + "<body><p>Parsed HTML into a doc.</p></body></html>";
+        Document doc = Jsoup.parse(html);
     }
 }
