ec65b0f9ca348ebc54a848a3a839692d5949b718
Panos Onasis <panos_onasis@hotmail.gr>
2019-05-01 12:52:21 +0300

    Menu and MenuItem Creation

---
+++ src/menu/MenuItem.java
@@ -0,0 +1,20 @@
+package menu;
+
+public class MenuItem
+{
+
+    public MenuItem(String string) 
+    {
+
+	}
+
+    public void setExitItem(boolean b) 
+    {
+	}
+
+    public boolean isExitItem() 
+    {
+		return false;
+	}
+
+}
