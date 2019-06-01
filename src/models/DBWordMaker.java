eb30303f64d3646bfb67bdeb8cedeb6d99180e27
KostasAronis <aronis_k@yahoo.com>
2019-06-01 16:47:54 +0300

    just for discussion

---
+++ src/models/DBWordMaker.java
@@ -0,0 +1,15 @@
+package models;
+import dbconnection.*;
+
+public class DBWordMaker implements IWordFactory
+{
+    IRepositoryFactory<Word, String> _repo;
+    public DBWordMaker(IRepositoryFactory<Word, String> repo){
+        _repo=repo;
+    }
+    @Override
+    public Word CreateWord(String word) {
+        Word wordModel = _repo.Insert(word);
+        return wordModel;
+    }
+}
